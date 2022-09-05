aeb6259b659d51b16d262d0f523f944f9c189c73
정현우 <junghw86@gmail.com>
2022-09-06 07:58:46 +0900

    TodoList에 관한 모든 template 구현 완료,
    css 를 두고 styled를 사용하는 방법이 도움이 될지 궁금하다.

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import TodoItem from "./TodoItem";
 
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  background: gray;
+  // background: gray;
 `;
 
 function TodoList() {
-  return <TodoListBlock>Todo List!</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text="프로젝트 생성하기" done={true} />
+      <TodoItem text="컴포넌트 생성하기" done={true} />
+      <TodoItem text="dRAST 생성하기" done={false} />
+      <TodoItem text="DST 생성하기" done={true} />
+      <TodoItem text="QID 생성하기" done={false} />
+    </TodoListBlock>
+  );
 }
 
 export default TodoList;
